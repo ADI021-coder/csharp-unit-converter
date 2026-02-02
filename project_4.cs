0d5026c03b3eb18a2beb0ea51e3863e7a2c4591c
Aditya Sasodey <asasodey21@gmail.com>
2026-02-02 20:14:12 +0000

    project 4 done!

---
+++ project_4.cs
@@ -0,0 +1,15 @@
+{
+    Console.WriteLine("Hello World");
+    Console.WriteLine("Sinple Unit Converter");
+
+    Console.Write("enter a number: ");
+    string userNum = Console.ReadLine();
+    double num = Convert.ToDouble(userNum);
+
+    double cmNum = (double) num*100;
+    double fNum = (double) num*3.28;
+    double iNum = (double) num*39.3;
+
+    Console.WriteLine("you entered " + num + "m which is " + cmNum + "cm and " + fNum + "feet and " + iNum + "inches");
+
+}
